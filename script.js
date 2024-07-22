33c4ff1548e3623663b7dc083fd54358e406e465
digital840 <kamranabdullah78910@gmail.com>
2024-07-22 14:57:42 +0500

    deploy

---
+++ script.js
@@ -0,0 +1,40 @@
+/*
+<div class="">
+        <section>
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Contacts</li>
+            <li>About</li>
+          </ul>
+          
+        </section>
+        <section>
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Contacts</li>
+            <li>About</li>
+          </ul>
+          
+        </section>
+        <section>
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Contacts</li>
+            <li>About</li>
+          </ul>
+          
+        </section>
+        <section>
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Contacts</li>
+            <li>About</li>
+          </ul>
+          
+        </section>
+      </section>
+*/
